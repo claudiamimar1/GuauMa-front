c7151b561b5b287b5923d4f0a814034773b7038a
Carolina <cmarinhincapie1@gmail.com>
2023-08-19 09:56:33 -0500

    Se crea navbar con las opciones inicio y blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/components/header/header.component';
 import { PaginaInicialComponent } from './feature/pagina-inicial/pagina-inicial.component';
+import { MenuComponent } from './core/components/menu/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PaginaInicialComponent
+    PaginaInicialComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
