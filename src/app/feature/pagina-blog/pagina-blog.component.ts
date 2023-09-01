b139a2194f151a42b1a87fc62b5c3b0b7bb87718
Carolina <cmarinhincapie1@gmail.com>
2023-09-01 01:24:10 -0500

    Se crean paginas de servicios y blog

--- src/app/feature/pagina-blog/pagina-blog.component.ts
+++ src/app/feature/pagina-blog/pagina-blog.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginaBlogComponent implements OnInit {
 
+  public datosBlog = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.cargarDatos();
+  }
+
+  private cargarDatos() {
+    this.datosBlog = [
+      {
+        'id' : 1,
+        'titulo': 'Perros en adopción',
+        'descripcion': 'En adopcion perros criollos',
+        'fechaPublicacion': `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`,
+        'estado': 'Activo',
+        'categoria': 'Adopción',
+        'imagen': './../../../assets/images/logo.png'
+      }, {
+        'id' : 2,
+        'titulo': 'Denuncia maltrato',
+        'descripcion': 'En la veterinaria X se presenta casos de maltrato a las mascotas',
+        'fechaPublicacion': `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`,
+        'estado': 'Activo',
+        'categoria': 'Denuncias',
+        'imagen': './../../../assets/images/logo.png'
+      }, {
+        'id' : 3,
+        'titulo': 'Promoción Spa',
+        'descripcion': 'Solo por el mes de Agosto, cualquiera de nuestros spa tendra el 10% de descuento',
+        'fechaPublicacion': `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`,
+        'estado': 'Activo',
+        'categoria': 'Promociones',
+        'imagen': './../../../assets/images/logo.png'
+      }
+    ];
   }
 
 }
--- src/app/feature/pagina-inicial/pagina-inicial.component.ts
+++ src/app/feature/pagina-inicial/pagina-inicial.component.ts
@@ -7,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginaInicialComponent implements OnInit {
 
+  public datosNegocios = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.cargarDatos();
+  }
+
+  private cargarDatos() {
+    this.datosNegocios = [
+      {
+        'id' : 1,
+        'nombre': 'San Blass',
+        'descripcion': 'Veterinaria',
+        'fechaPublicacion': `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`,
+        'estado': 'Activo',
+        'imagen': './../../../assets/images/logo.png'
+      },
+      {
+        'id' : 2,
+        'nombre': 'Oh, my pets',
+        'descripcion': 'Tienda',
+        'fechaPublicacion': `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`,
+        'estado': 'Activo',
+        'imagen': './../../../assets/images/logo.png'
+      },
+      {
+        'id' : 3,
+        'nombre': 'Paseadora',
+        'descripcion': 'Tienda',
+        'fechaPublicacion': `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`,
+        'estado': 'Activo',
+        'imagen': './../../../assets/images/logo.png'
+      },
+      {
+        'id' : 3,
+        'nombre': 'Paseadora',
+        'descripcion': 'Paseo perros de lunes a viernes',
+        'fechaPublicacion': `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`,
+        'estado': 'Activo',
+        'imagen': './../../../assets/images/logo.png'
+      }
+    ];
   }
 
 }
