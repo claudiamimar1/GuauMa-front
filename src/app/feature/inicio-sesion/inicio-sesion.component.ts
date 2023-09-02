b4f6003c148946fe68ede7d268b59964cabdf592
Carolina <cmarinhincapie1@gmail.com>
2023-09-02 10:17:03 -0500

    Se modifica la pantalla de perfil

--- src/app/feature/inicio-sesion/inicio-sesion.component.ts
+++ src/app/feature/inicio-sesion/inicio-sesion.component.ts
@@ -16,14 +16,21 @@ export class InicioSesionComponent implements OnInit {
 
   ngOnInit(): void {
     this.auth.isAuthenticated$.subscribe(isAuthenticated => {
-      if(isAuthenticated) {
+      if(isAuthenticated && !this.consultarUsuario()) {
         this.router.navigate(['/registro-datos']);
+      } else if (isAuthenticated && this.consultarUsuario()) {
+        this.router.navigate(['/inicio-cuidado-animal']);
       }
     });
   }
 
   public iniciarSesion() {
     this.auth.loginWithRedirect();
+  }
+
+  public consultarUsuario() {
+    // Se consume servicio para saber si el usuario existe
+    return true;
   }
 
 }
--- src/app/feature/perfil/perfil.component.ts
+++ src/app/feature/perfil/perfil.component.ts
@@ -9,6 +9,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class PerfilComponent implements OnInit {
 
   public datosPerfil: FormGroup;
+  public noEditar: boolean = true;
 
   constructor() { }
 
@@ -18,11 +19,21 @@ export class PerfilComponent implements OnInit {
 
   private cargarDatos() {
     this.datosPerfil = new FormGroup({
-      nombres: new FormControl('Carolina', Validators.maxLength(20)),
-      apellidos: new FormControl('Marín', Validators.maxLength(30)),
-      correoElectronico: new FormControl('cmarinh@uqvirtual.edu.co', Validators.email),
-      numeroContacto: new FormControl('3116595201', [Validators.pattern(/^\d+$/), Validators.maxLength(10)])
+      razonSocial: new FormControl('Carolina Marín Hincapié', Validators.required),
+      tipoDocumento: new FormControl('C', Validators.required),
+      numeroDocumento: new FormControl('1094957383', [Validators.pattern(/^\d+$/), Validators.required]),
+      correoElectronico: new FormControl('cmarinh@uqvirtual.edu.co'),
+      rol: new FormControl('consumidor', Validators.required),
+      numeroContacto: new FormControl('3116595201', [Validators.pattern(/^\d+$/), Validators.required]),
+      departamento: new FormControl('Quindío', Validators.required),
+      ciudad: new FormControl('Armenia', Validators.required),
+      direccion: new FormControl('Carrera 6 #19-25', Validators.required)
     });
+  }
+
+  public editarDatosUsuario() {
+    debugger;
+    this.noEditar = false;
   }
 
 }
