3b9c31764c2a8c3c8eded45eada13a7a899b767b
carolina.marin <cmarinhincapie1@gmail.com>
2023-09-02 20:39:48 -0500

    Se modifica la pagina del perfil para editar los datos

--- src/app/feature/perfil/perfil.component.ts
+++ src/app/feature/perfil/perfil.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
@@ -9,7 +9,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class PerfilComponent implements OnInit {
 
   public datosPerfil: FormGroup;
-  public noEditar: boolean = true;
+  public editarCampos: boolean = false;
 
   constructor() { }
 
@@ -19,21 +19,41 @@ export class PerfilComponent implements OnInit {
 
   private cargarDatos() {
     this.datosPerfil = new FormGroup({
-      razonSocial: new FormControl('Carolina Marín Hincapié', Validators.required),
-      tipoDocumento: new FormControl('C', Validators.required),
-      numeroDocumento: new FormControl('1094957383', [Validators.pattern(/^\d+$/), Validators.required]),
-      correoElectronico: new FormControl('cmarinh@uqvirtual.edu.co'),
-      rol: new FormControl('consumidor', Validators.required),
-      numeroContacto: new FormControl('3116595201', [Validators.pattern(/^\d+$/), Validators.required]),
-      departamento: new FormControl('Quindío', Validators.required),
-      ciudad: new FormControl('Armenia', Validators.required),
-      direccion: new FormControl('Carrera 6 #19-25', Validators.required)
+      razonSocial: new FormControl({value: 'Carolina Marín Hincapié', disabled: true}, Validators.required),
+      tipoDocumento: new FormControl({value: 'C', disabled: true}, Validators.required),
+      numeroDocumento: new FormControl({value: '1094957383', disabled: true}, [Validators.pattern(/^\d+$/), Validators.required]),
+      correoElectronico: new FormControl({value: 'cmarinh@uqvirtual.edu.co', disabled: true}),
+      rol: new FormControl({value: 'consumidor', disabled: true}, Validators.required),
+      numeroContacto: new FormControl({value: '3116595201', disabled: true}, [Validators.pattern(/^\d+$/), Validators.required]),
+      departamento: new FormControl({value: 'Quindío', disabled: true}, Validators.required),
+      ciudad: new FormControl({value: 'Armenia', disabled: true}, Validators.required),
+      direccion: new FormControl({value: 'Carrera 6 #19-25', disabled: true}, Validators.required)
     });
   }
 
   public editarDatosUsuario() {
-    debugger;
-    this.noEditar = false;
+    this.datosPerfil.get('razonSocial').enable();
+    this.datosPerfil.get('rol').enable();
+    this.datosPerfil.get('numeroContacto').enable();
+    this.datosPerfil.get('departamento').enable();
+    this.datosPerfil.get('ciudad').enable();
+    this.datosPerfil.get('direccion').enable();
+    this.editarCampos = true;
+  }
+
+  public cancelarModificacion() {
+    this.datosPerfil.get('razonSocial').disable();
+    this.datosPerfil.get('rol').disable();
+    this.datosPerfil.get('numeroContacto').disable();
+    this.datosPerfil.get('departamento').disable();
+    this.datosPerfil.get('ciudad').disable();
+    this.datosPerfil.get('direccion').disable();
+    this.editarCampos = false;
+  }
+
+  public guardarDatosNuevos() {
+    this.cancelarModificacion();
+    this.editarCampos = false;
   }
 
 }
